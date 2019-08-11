d4c19175d9e75daecdce04bb5e3368d2cf4e8c47
Daniel <dpericich@gmail.com>
2019-08-11 00:16:25 -0500

    2nd commit

--- RStudio-Github.R
+++ RStudio-Github.R
@@ -1,3 +1,7 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+
+print("This is my second commit on this file")
+
+print("Getting Slik was a pain in the butt, but I'm glad I have it now")
